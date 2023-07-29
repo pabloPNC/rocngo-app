f3cbc259a5d04cb4b5eab07140f7b0e466376860
pablo navarro carpio <pablo.navarrocarpio@gmail.com>
2023-07-29 18:51:44 +0200

    Fast colon dataset added

--- modules/plotsPage/compareUi.R
+++ modules/plotsPage/compareUi.R
@@ -1,23 +1,30 @@
 
 compareContents <- list(
-    h1("Compare"),
-    box(
-        title = "Selector",
-        pickerInput(
-          inputId = "graphicSelector",
-          label = "Select plots to show",
-          choices = NULL,
-          multiple = TRUE
+    fluidRow(
+        column(
+            width = 6,
+            box(
+                title = "Selector",
+                width = NULL,
+                pickerInput(
+                inputId = "graphicSelector",
+                label = "Select plots to show",
+                choices = NULL,
+                multiple = TRUE
+                ),
+                textInput(
+                    label = "Introduce plot name",
+                    inputId = "combinedPlotName",
+                ),
+                actionButton(
+                    inputId = "plotButton",
+                    label = "Plot!",
+                    icon = icon("check"),
+                    class = "btn-block btn-success"
+                )
+            ),
+            offset = 3
         ),
-        textInput(
-            label = "Introduce plot name",
-            inputId = "combinedPlotName",
-        ),
-        actionButton(
-            inputId = "plotButton",
-            label = "Plot!",
-            icon = icon("check"),
-            class = "btn-block btn-success"
-        )
+        style = "padding-top: 30px"
     )
 )
